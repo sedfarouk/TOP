3cd2fbb190a33b2ac726a7d2e7ca8751e356fe26
Farouk Sedick <sadikalhanssah@gmail.com>
2023-12-27 16:48:00 +0000

    first js exercise on DOM manipulation and events

---
+++ javascript_exercises/js_exercise/index.js
@@ -0,0 +1,23 @@
+const div = document.querySelector('#container');
+
+/* add <p> with red text */
+const para = document.createElement('p');
+para.textContent = "Hey! I'm red";
+para.style.color = 'red';
+div.appendChild(para);
+
+/* add <h3> with blue text */
+const head = document.createElement('h3');
+head.textContent = "I'm a blue h3!"
+head.style.color = 'blue';
+div.appendChild(head);
+
+/* div with black border and pink bg color */
+const subDiv = document.createElement('div');
+subDiv.innerHTML = `<h1>I'm in a div</h1>   
+                    <p>ME TOO!</p>`
+subDiv.setAttribute('style', 'border:3px solid black; background-color:pink');
+div.appendChild(subDiv);
+
+/* NB: Though not safe, the innerHTML attribute is used here to 
+keep things tidy and neat*/
