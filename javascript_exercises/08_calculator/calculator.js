37dbc5e61336b98d4cb8f2b4a23a1ba913ca81b4
Far9999 <sadikalhanssah@gmail.com>
2024-01-02 21:00:04 +0000

    finish challenge 8 of js

---
+++ javascript_exercises/08_calculator/calculator.js
@@ -0,0 +1,33 @@
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const sum = function(numbers = [...args]) {
+	return sum(numbers);
+};
+
+const multiply = function(a, b) {
+    return a * b;
+};
+
+const power = function(base, exp) {
+	return Math.pow(base, exp);
+};
+
+const factorial = function(num) {
+	return Math.factorial(num);
+};
+
+// Do not edit below this line
+module.exports = {
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial
+};
